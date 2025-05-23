a5faf2171f4507bd7167c201f338a9a3bba0131d
SanjivTezo <sanjiv.s@tezo.com>
2025-05-23 14:30:30 +0530

    Enhance TodoList and style for better layout and usability

--- typescript-tutorial/src/components/TodoList.tsx
+++ typescript-tutorial/src/components/TodoList.tsx
@@ -1,26 +1,40 @@
-import React from 'react'
-import "./style.css"
-import { Todo } from '../model'
-import SingleTodo from './SingleTodo';
+import React from "react";
+import "./style.css";
+import { Todo } from "../model";
+import SingleTodo from "./SingleTodo";
 
 interface Props {
-  todos:Todo[];
+  todos: Todo[];
   setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
-  }
-
-const TodoList:React.FC<Props> = ({todos,setTodos}:Props) => {
-  return (
-    <div className='todos'>
-      {todos.map((todo) => (
-        <SingleTodo
-          todo={todo}
-          key={todo.id}
-          todos={todos}
-          setTodos={setTodos}
-        />  
-      ))}
-    </div>
-  )
 }
 
-export default TodoList
+const TodoList: React.FC<Props> = ({ todos, setTodos }: Props) => {
+  return (
+    <div className="container">
+      <div className="todos">
+        <span className="todos_heading">Active Task</span>
+        {todos.map((todo) => (
+          <SingleTodo
+            todo={todo}
+            key={todo.id}
+            todos={todos}
+            setTodos={setTodos}
+          />
+        ))}
+      </div>
+      <div className="todos remove">
+        <span className="todos_heading">Completed task</span>
+        {todos.map((todo) => (
+          <SingleTodo
+            todo={todo}
+            key={todo.id}
+            todos={todos}
+            setTodos={setTodos}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default TodoList;
